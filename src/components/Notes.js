111a07bd547de3776dd0bcaa3d0f832bb3926cd9
arsalan_qadir <arsalan.qadir@otwosoft.com>
2023-03-06 12:35:00 +0500

    use of sx props by  external file

--- src/components/Notes.js
+++ src/components/Notes.js
@@ -1,12 +1,13 @@
 import { Button, Typography } from "@mui/material";
 import React from "react";
-
+import { styles } from "./notesStyles";
 const Notes = () => {
   return (
     <div>
       <Typography variant="pTage">Notes</Typography>
       <Typography color="primary">Notes</Typography>
       <Typography variant="h2">Notes</Typography>
+      <Typography sx={styles}>Notes</Typography>
       <Button color="primary" variant="contained" type="submit">
         Submit
       </Button>
--- src/components/notesStyles.js
+++ src/components/notesStyles.js
@@ -1,3 +1,10 @@
-const styles={
-    
-}
+const styles = {
+  color: "pink",
+  fontSize: "12px",
+};
+ const styles2={
+
+ }
+export  { styles, styles2 };
+
+
