473a02500c4b12784d41411d5a13cbdcc610a072
arsalan_qadir <arsalan.qadir@otwosoft.com>
2023-03-06 12:24:48 +0500

    use of createTheme and ThemeProvide to style in mui component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import Notes from "./components/Notes";
+import { createTheme, ThemeProvider } from "@mui/material";
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#ec313b",
+    },
+  },
+  typography: {
+    h1: {
+      color: "red",
+      fontSize: "40px",
+    },
+    h2: {
+      color: "green",
+      fontSize: "20px",
+    },
+    pTage:{
+      color:"yellow"
+    }
+  },
+});
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ThemeProvider theme={theme}>
+        <Notes />
+      </ThemeProvider>
     </div>
   );
 }
---
+++ src/components/Notes.js
@@ -0,0 +1,17 @@
+import { Button, Typography } from "@mui/material";
+import React from "react";
+
+const Notes = () => {
+  return (
+    <div>
+      <Typography variant="pTage">Notes</Typography>
+      <Typography color="primary">Notes</Typography>
+      <Typography variant="h2">Notes</Typography>
+      <Button color="primary" variant="contained" type="submit">
+        Submit
+      </Button>
+    </div>
+  );
+};
+
+export default Notes;
